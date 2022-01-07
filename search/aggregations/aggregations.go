f733f01124d7bc421c28ec46fc63a60c9457e346
Minoru Osuka <minoru.osuka@gmail.com>
2022-01-07 10:04:49 +0900

    Refactor aggregations (#55)

--- search/aggregations/aggregations.go
+++ search/aggregations/aggregations.go
@@ -1,6 +1,12 @@
 package aggregations
 
-import "sort"
+import (
+	"encoding/json"
+	"sort"
+
+	"github.com/blugelabs/bluge/search"
+	"github.com/mosuka/phalanx/proto"
+)
 
 type AggregationType int
 
@@ -38,6 +44,92 @@ var (
 		"avg":        AggregationTypeAvg,
 	}
 )
+
+func NewAggregations(requests map[string]*proto.AggregationRequest) (map[string]search.Aggregation, error) {
+	aggs := make(map[string]search.Aggregation)
+	for name, request := range requests {
+		switch request.Type {
+		case AggregationType_name[AggregationTypeTerms]:
+			opts := make(map[string]interface{})
+			if err := json.Unmarshal(request.Options, &opts); err != nil {
+				return nil, err
+			}
+			agg, err := NewTermsAggregationWithOptions(opts)
+			if err != nil {
+				return nil, err
+			}
+
+			aggs[name] = agg
+		case AggregationType_name[AggregationTypeRange]:
+			opts := make(map[string]interface{})
+			if err := json.Unmarshal(request.Options, &opts); err != nil {
+				return nil, err
+			}
+			agg, err := NewRangeAggregationWithOptions(opts)
+			if err != nil {
+				return nil, err
+			}
+
+			aggs[name] = agg
+		case AggregationType_name[AggregationTypeDateRange]:
+			opts := make(map[string]interface{})
+			if err := json.Unmarshal(request.Options, &opts); err != nil {
+				return nil, err
+			}
+			agg, err := NewDateRangeAggregationWithOptions(opts)
+			if err != nil {
+				return nil, err
+			}
+
+			aggs[name] = agg
+		case AggregationType_name[AggregationTypeSum]:
+			opts := make(map[string]interface{})
+			if err := json.Unmarshal(request.Options, &opts); err != nil {
+				return nil, err
+			}
+			agg, err := NewSumWithOptions(opts)
+			if err != nil {
+				return nil, err
+			}
+
+			aggs[name] = agg
+		case AggregationType_name[AggregationTypeMin]:
+			opts := make(map[string]interface{})
+			if err := json.Unmarshal(request.Options, &opts); err != nil {
+				return nil, err
+			}
+			agg, err := NewMinWithOptions(opts)
+			if err != nil {
+				return nil, err
+			}
+
+			aggs[name] = agg
+		case AggregationType_name[AggregationTypeMax]:
+			opts := make(map[string]interface{})
+			if err := json.Unmarshal(request.Options, &opts); err != nil {
+				return nil, err
+			}
+			agg, err := NewMaxWithOptions(opts)
+			if err != nil {
+				return nil, err
+			}
+
+			aggs[name] = agg
+		case AggregationType_name[AggregationTypeAvg]:
+			opts := make(map[string]interface{})
+			if err := json.Unmarshal(request.Options, &opts); err != nil {
+				return nil, err
+			}
+			agg, err := NewAvgWithOptions(opts)
+			if err != nil {
+				return nil, err
+			}
+
+			aggs[name] = agg
+		}
+	}
+	return aggs, nil
+}
 
 func SortByCount(values map[string]float64) PairList {
 	pl := make(PairList, len(values))
